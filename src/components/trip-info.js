fb9453ca34ed69a23730906b5938bc0b4ddc00a9
Tanya <45432959+tkoshurnikova@users.noreply.github.com>
2020-01-02 15:01:54 +0300

    Маршрут поездки по ТЗ

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -9,14 +9,32 @@ const getTripCost = (card) => {
   return tripCost;
 };
 
+const getRouteInfo = (cards) => {
+  const cities = cards.map((card) => card.destination);
+
+  switch (cities.length) {
+    case 1:
+    case 2:
+      return cities[0] + ` &mdash; ` + cities[1];
+
+    case 3:
+      return cities[0] + cities[1] + cities[2];
+
+    default:
+      return cities[0] + ` &mdash; ... &mdash; ` + cities[cities.length - 1];
+  }
+};
+
 const createTripInfoTemplate = (cards) => {
   const totalCost = cards.map((card) => getTripCost(card)).reduce((sum, current) => {
     return sum + current;
   });
+  const route = getRouteInfo(cards);
+
   return (
     `<section class="trip-main__trip-info  trip-info">
       <div class="trip-info__main">
-        <h1 class="trip-info__title">${cards[0].destination} &mdash; ... &mdash; ${cards[cards.length - 1].destination}</h1>
+        <h1 class="trip-info__title">${route}</h1>
         <p class="trip-info__dates">${MONTHS[cards[0].startDate.getMonth()]} ${cards[0].startDate.getDate()}&nbsp;&mdash;&nbsp;${cards[cards.length - 1].endDate.getDate()}</p>
       </div>
       <p class="trip-info__cost">
